9943ceb2edb6606cafb9ca153877a37439f7830a
sofia <sofigerenacortes@gmail.com>
2023-06-27 13:24:32 -0500

    Initial Commit

---
+++ GLOBALGLIMPSE/GLOBALGLIMPSEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLOBALGLIMPSEApp.swift
+//  GLOBALGLIMPSE
+//
+//  Created by Monitor #5 on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GLOBALGLIMPSEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
